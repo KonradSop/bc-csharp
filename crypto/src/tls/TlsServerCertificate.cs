e2d6418de0de9825a3f09d469c31592933818b66
Peter Dettman <peter.dettman@bouncycastle.org>
2026-08-25 20:35:26 +0700

    Add TODO

--- crypto/src/tls/TlsServerCertificate.cs
+++ crypto/src/tls/TlsServerCertificate.cs
@@ -3,6 +3,7 @@
 namespace Org.BouncyCastle.Tls
 {
     /// <summary>Server certificate carrier interface.</summary>
+    // TODO[api] Turn this into a concrete sealed class (record)
     public interface TlsServerCertificate
     {
         Certificate Certificate { get; }
